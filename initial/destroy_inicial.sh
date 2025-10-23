ddd0525145d003fa6c7e5ca3d4b97f41d74faeb9
Younyat <assouyat1@gmail.com>
2025-10-23 16:32:15 +0200

    se ha adaptado destory_inicial.sh ya que este último esta adaptado al contatxo scenario

--- initial/destroy_inicial.sh
+++ initial/destroy_inicial.sh
@@ -5,36 +5,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# ===============================
-# 🔥 Script: destroy_scenario.sh
-# 🧩 Ubicación: /scenario/
-# 🧨 Destruye todos los recursos Terraform generados en /tf_out/
-# ===============================
 
-# Ruta absoluta del directorio tf_out (ajústala si cambia la estructura)
-TF_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/../tf_out" && pwd)"
-
-# Comprobación básica
-if [ ! -d "$TF_DIR" ]; then
-  echo "❌ No se encontró el directorio de despliegue: $TF_DIR"
-  exit 1
-fi
-
-echo "==============================================="
-echo "🧨 Iniciando destrucción del escenario Terraform"
-echo "📁 Directorio: $TF_DIR"
-echo "==============================================="
-
-
-# Entrar en el directorio tf_out
-cd "$TF_DIR"
 
 # Inicializar Terraform si es necesario
 if [ ! -d ".terraform" ]; then
   echo "⚙️  Ejecutando 'terraform init'..."
   terraform init -input=false
 fi
-
+terraform init -upgrade
 # Destruir todos los recursos
 echo "🚀 Ejecutando 'terraform destroy'..."
 terraform destroy -auto-approve -parallelism=4
